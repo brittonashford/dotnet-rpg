4be6383c14d731385a255138473ea2b534eb7925
Bret Kitchell <brittonashford@gmail.com>
2022-10-29 15:51:52 -0500

    added GetAllCharacter and GetCharacter to CharacterController

--- Models/Character.cs
+++ Models/Character.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace dotnet_rpg.vscode.Models
+namespace dotnet_rpg.Models
 {
     public class Character
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; } = "Frodo";
-        public int HitPoints { get; set; } = 100;
+        public int HitPoints { get; set; }  = 100;
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
-        public RpgClass Class { get; set; }  = RpgClass.Knight;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+
     }
 }
