03e7e7e2bd2aa84b5d1dcd3007911f9dc91cca7f
Bret Kitchell <brittonashford@gmail.com>
2022-11-03 21:26:37 -0500

    added Authentication interface, repository, and controller

---
+++ Migrations/20221103235855_UserCharacterRelationship.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class UserCharacterRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Characters",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_UserId",
+                table: "Characters",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_UserId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Characters");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+        }
+    }
+}
