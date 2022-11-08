4092e956121ed2ac5bbd5037fabb9c8d5d667d25
Bret Kitchell <brittonashford@gmail.com>
2022-11-07 21:06:50 -0600

    added skill attack functionality

--- Services/CharacterService/FightService/IFightService.cs
+++ Services/CharacterService/FightService/IFightService.cs
@@ -9,5 +9,7 @@ namespace dotnet_rpg.Services.CharacterService.FightService
     public interface IFightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+        Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
+        Task<ServiceResponse<AttackResultDto>> Fight(FightRequestDto request);
     }
 }
